3de5fd2b4119266e3122831b28c19b6bae829ff7
Lian Welch <tuq36167@temple.edu>
2026-02-19 20:24:41 -0500

    management between backend and front end for image upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 //testing supabase
 import './lib/supabase'
 
+import ImageUploader from "./components/ImageUploader";
+
 function App() {
   const [page, setPage] = useState("menu");
 
@@ -41,6 +43,10 @@ function ReaderPage({ onBack }) {
       <div className="readerBox">
         Storybook will show here later.
       </div>
+
+      {/* Testing the image uploader*/}
+      <h2>Image Upload Test</h2>
+      <ImageUploader />
     </div>
   );
 }
