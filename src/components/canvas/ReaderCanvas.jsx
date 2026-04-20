b22347f7e9231496c38a03c396ddb93b5f8ca9c1
RolandG369 <tup55624@temple.edu>
2026-04-20 12:33:59 -0400

    Update Opacity

--- src/components/canvas/ReaderCanvas.jsx
+++ src/components/canvas/ReaderCanvas.jsx
@@ -67,10 +67,10 @@ function ReadOnlyHotspot({ hotspot, scale, canvasW, canvasH }) {
     onMouseEnter: () => setIsHovering(true),
     onMouseLeave: () => setIsHovering(false),
     onClick: () => speakWord(word), //TTS
-    fill: isHovering ? "#ff69b4" : "#ff69b4",
-    opacity: isHovering ? 0.4 : 0.15,
+    fill: "transparent",
     stroke: "#ff1493",
-    strokeWidth: isHovering ? 5 : 3,
+    strokeWidth: isHovering ? 3 : 2,
+    opacity: isHovering ? 0.8 : 0.4,
   };
 
   if (shape_type === "circle") {
